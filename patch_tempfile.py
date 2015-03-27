82ca7837effb975a946f4a5e0caf722f5ba72894
Robin Wilson <robin@rtwilson.com>
2015-03-27 12:36:03 +0000

    Restructured into proper module

--- patch_tempfile.py
+++
@@ -1,7 +0,0 @@
-from wrapt import wrap_function_wrapper
-def _mkdtemp_wrapper(wrapped, instance, args, kwargs):
-    print 'calling', wrapped.__name__
-    return wrapped(*args, **kwargs)
-def apply_patch(module):
-    print 'patching', module.__name__
-    wrap_function_wrapper(module, 'mkdtemp', _mkdtemp_wrapper)
