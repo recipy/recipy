e314191c82cfc17de8cdd079f9ab3c75d89227c0
Robin Wilson <robin@rtwilson.com>
2015-03-27 10:50:24 +0000

    Even more generic-ness: nearly ready to put all in one class with just attributes!

--- PatchPandas.py
+++ PatchPandas.py
@@ -6,24 +6,25 @@ from log import *
 from utils import *
 
 class PatchPandas(PatchImporter):
+    input_functions = ['read_csv', 'read_table', 'read_excel', 'read_hdf', 'read_pickle', 'read_stata']
+    output_functions = ['DataFrame.to_csv']
+
+
 
     def patch(self, mod):
-        input_functions = ['read_csv', 'read_table', 'read_excel', 'read_hdf', 'read_pickle', 'read_stata']
-        for f in input_functions:
-            self.patch_function(mod, f, self.log_pandas_read)
+        for f in self.input_functions:
+            patch_function(mod, f, create_wrapper(log_input, 0, 'pandas'))
 
-        output_functions = ['DataFrame.to_csv', ]
-        self.patch_function(mod, , self.log_pandas_write) 
+        for f in self.output_functions:
+            patch_function(mod, f, create_wrapper(log_output, 0, 'pandas')) 
         return mod
 
-    @wrapt.decorator
-    def log_pandas_read(self, wrapped, instance, args, kwargs):
-        log_input(args[0], 'pandas')
-        return wrapped(*args, **kwargs)
 
-    @wrapt.decorator
-    def log_pandas_write(self, wrapped, instance, args, kwargs):
-        log_output(args[0], 'pandas')
-        return wrapped(*args, **kwargs)
+    
+
+    # @wrapt.decorator
+    # def log_pandas_write(self, wrapped, instance, args, kwargs):
+    #     log_output(args[0], 'pandas')
+    #     return wrapped(*args, **kwargs)
 
 sys.meta_path = [PatchPandas()]
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import wrapt
+
 def recursive_getattr(obj, attr):
     prev_part = obj
 
@@ -13,3 +15,17 @@ def recursive_setattr(obj, attr, value):
         prev_part = getattr(prev_part, part)
 
     setattr(prev_part, attr.split(".")[-1], value)
+
+def patch_function(mod, function, wrapper):
+        old_f_name = '_%s' % function.replace(".", "_")
+        setattr(mod, old_f_name, recursive_getattr(mod, function))
+
+        recursive_setattr(mod, function, wrapper(getattr(mod, old_f_name)))
+
+def create_wrapper(function, arg_loc, source):
+    @wrapt.decorator
+    def f(wrapped, instance, args, kwargs):
+        function(args[arg_loc], source)
+        return wrapped(*args, **kwargs)
+
+    return f
