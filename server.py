c3f1ce098b0157aedd5b77ab305270b24b27173b
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-04-28 22:08:06 +0200

    Open gui in browser when the server is started
    Refs #6
    The server is started on a random port (between 5000 and 5999).
    If the browser is not open yet, the browser is opened (automatically).
    
    The random port needs to be changed to the first port (above 4999) that
    is not currently used.

--- server.py
+++ server.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
 from recipyGui import recipyGui
+import random, threading, webbrowser
 
-recipyGui.run(debug = True)
+port = 5000 + random.randint(0, 999)
+url = "http://127.0.0.1:{0}".format(port)
+
+# Give the application some time before it starts
+threading.Timer(1.25, lambda: webbrowser.open(url) ).start()
+
+recipyGui.run(debug = True, port=port)
