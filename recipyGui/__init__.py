307253bcdea74440e6e26b119f30d42d61c0fe17
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-08-09 19:12:19 +0200

    Add filter to color git diff
    Example method to generate html taken from:
    http://stackoverflow.com/questions/2053657/git-diff-to-html
    
    Closes #5.

--- recipyGui/__init__.py
+++ recipyGui/__init__.py
@@ -55,7 +55,24 @@ def gitorigin2url(origin):
     url = origin.replace(':', '/')
     url = url.replace('git@', 'http://')
     url = url.replace('.git', '')
-    
+
     return url
 
 recipyGui.jinja_env.filters['gitorigin2url'] = gitorigin2url
+
+@recipyGui.template_filter()
+def colordiff(diff):
+    """convert git diff data to html/bootstrap color code"""
+    if diff=='':
+        return ''
+    diff = diff.strip()
+    diff = diff.replace('\n', '&nbsp;\n')
+    diffData = diff.split('\n')
+    print diffData
+    openTag = '<tr><td class="'
+    openTagEnd = '">'
+    nbsp = '&nbsp;&nbsp;&nbsp;&nbsp;'
+    data = '\n'.join([('%s%s%s%s<samp>%s</samp><td></tr>' % (openTag, 'bg-danger' if line.startswith('-') else ('bg-success' if line.startswith('+') else ('bg-info' if line.startswith('@') else '')), openTagEnd, nbsp*line.count('\t') ,line)) for line in diffData])
+    return '<table width="100%">'+data+'</table>'
+
+recipyGui.jinja_env.filters['colordiff'] = colordiff
