08500cc12fbd0eb68c62de43f629d3f9a86ad5b0
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-08-09 10:04:23 +0200

    Add comment about reloading the app
    When the app is started with debug=True, two tabs are opened. This can
    be fixed by setting debug=False. Added a comment to remember this.
    
    Closes #7.

--- recipy-gui.py
+++ recipy-gui.py
@@ -15,4 +15,6 @@ url = "http://127.0.0.1:{0}".format(port)
 # Give the application some time before it starts
 threading.Timer(1.25, lambda: webbrowser.open(url) ).start()
 
+# Turn off reloading by setting debug = False (this also fixes starting the
+# application twice)
 recipyGui.run(debug = True, port=port)
