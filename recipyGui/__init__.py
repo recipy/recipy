97982eeabe622b4b52dd04c3d0a10a7c912354b2
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-08-14 18:36:44 +0200

    Remove mongo

--- recipyGui/__init__.py
+++ recipyGui/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, url_for
 import os
-from flask.ext.pymongo import PyMongo
 from flask_bootstrap import Bootstrap
 import re
 
@@ -11,11 +10,6 @@ Bootstrap(recipyGui)
 
 # Determines the destination of the build. Only usefull if you're using Frozen-Flask
 recipyGui.config['FREEZER_DESTINATION'] = os.path.dirname(os.path.abspath(__file__))+'/../build'
-
-# MongoDB settings
-recipyGui.config['MONGO_DBNAME'] = 'recipyDB'
-
-mongo = PyMongo(recipyGui)
 
 # Function to easily find your assets
 # In your template use <link rel=stylesheet href="{{ static('filename') }}">
--- recipyGui/views.py
+++ recipyGui/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, render_template
-from recipyGui import recipyGui, mongo
+from recipyGui import recipyGui
 from forms import SearchForm
 from bson.objectid import ObjectId
 
@@ -14,13 +14,15 @@ def index():
 
     if not query:
         # Return all runs, ordered by date (oldest run first)
-        runs = [r for r in mongo.db.recipies.find({}).sort('date', -1)]
+        #runs = [r for r in mongo.db.recipies.find({}).sort('date', -1)]
+        runs =[]
     else:
         # Search runs using the query string
-        q = { '$text': { '$search': query} }
-        score = { 'score': { '$meta': 'textScore' } }
-        score_sort = [('score', {'$meta': 'textScore'})]
-        runs = [r for r in mongo.db.recipies.find(q, score).sort(score_sort)]
+        #q = { '$text': { '$search': query} }
+        #score = { 'score': { '$meta': 'textScore' } }
+        #score_sort = [('score', {'$meta': 'textScore'})]
+        #runs = [r for r in mongo.db.recipies.find(q, score).sort(score_sort)]
+        runs = []
 
     return render_template('list.html', runs=runs, query=query, form=form)
 
@@ -32,6 +34,7 @@ def run_details():
         run_id = request.args.get('id', '')
 
         q = { '_id': ObjectId(run_id) }
-        r = mongo.db.recipies.find_one(q)
+        #r = mongo.db.recipies.find_one(q)
+        r = None
 
         return render_template('details.html', query=query, form=form, run=r)
