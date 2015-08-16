3cc4031341be50b1ed0b92ff1f9d17c965fd0687
Robin Wilson <robin@rtwilson.com>
2015-08-16 15:31:38 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='development, science',
+    keywords='development, science, reproducibility, provenance',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
