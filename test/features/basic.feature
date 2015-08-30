5368417b41015578ed26530416c1573a436de286
Robin Wilson <robin@rtwilson.com>
2015-08-30 18:07:06 +0100

    Added behave test for running recipy as a module

--- test/features/basic.feature
+++ test/features/basic.feature
@@ -4,3 +4,8 @@ Scenario: Run a simple test
     Given we have recipy set up for testing
     When we run some code
     Then an entry should be added to the database
+
+Scenario: Test running recipy as a module
+	Given we have recipy set up for testing
+	When we run some code as a module
+	Then an entry should be added to the database
--- test/features/steps/basic.py
+++ test/features/steps/basic.py
@@ -4,7 +4,7 @@ import os, sys
 import subprocess
 
 from behave_utils import ( setup_testing_environment, check_id_exists_in_db,
-						   run_script_and_get_id )
+                           run_script_and_get_id )
 
 
 @given('we have recipy set up for testing')
@@ -15,6 +15,11 @@ def step_impl(context):
 def step_impl(context):
     context.run_id = run_script_and_get_id('example_script2.py')
 
+@when('we run some code as a module')
+def step_impl(context):
+    output = subprocess.check_output([sys.executable, '-m', 'recipy', 'example_script3.py', 'test.npy'], env={'PYTHONPATH': os.path.abspath('../')}).decode('utf-8')
+    context.run_id = output.replace('recipy run inserted, with ID', '').strip()
+
 @then('an entry should be added to the database')
 def step_impl(context):
-	check_id_exists_in_db(context.run_id, context.db_file)
+    check_id_exists_in_db(context.run_id, context.db_file)
