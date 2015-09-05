080b5e15e58f8781afdb59c65d49742ed620894c
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-09-05 21:01:41 +0200

    Add view to return pretty printed json of runs
    Added a view that returns a text-response that displays pretty printed
    json of the runs. Also works for search results.

--- recipyGui/views.py
+++ recipyGui/views.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, request, render_template, redirect, url_for, \
-    escape
+    escape, make_response
 from recipyGui import recipyGui
 from .forms import SearchForm, AnnotateRunForm
 from tinydb import TinyDB, where
 import re
 from dateutil.parser import parse
 import os
+from ast import literal_eval
+from json import dumps
 
 
 routes = Blueprint('routes', __name__, template_folder='templates')
@@ -30,13 +32,17 @@ def index():
             where('outputs').any(lambda x: re.match(".+%s.+" % query, x)) |
             where('notes').contains(query))
     runs = sorted(runs, key = lambda x: parse(x['date'].replace('{TinyDate}:', '')) if x['date'] is not None else x['eid'], reverse=True)
+
+    run_ids = []
     for run in runs:
         if 'notes' in run.keys():
             run['notes'] = str(escape(run['notes']))
+        run_ids.append(run.eid)
 
     db.close()
 
-    return render_template('list.html', runs=runs, query=query, form=form)
+    return render_template('list.html', runs=runs, query=query, form=form,
+                           run_ids=str(run_ids))
 
 
 @recipyGui.route('/run_details')
@@ -83,3 +89,14 @@ def annotate():
     db.close()
 
     return redirect(url_for('run_details', id=run_id, query=query))
+
+@recipyGui.route('/runs2json', methods=['POST'])
+def runs2json():
+    run_ids = literal_eval(request.form['run_ids'])
+    db = TinyDB(recipyGui.config.get('tinydb'))
+    runs = [db.get(eid=run_id) for run_id in run_ids]
+    db.close()
+
+    response = make_response(dumps(runs, indent=2, sort_keys=True))
+    response.headers["content-type"] = "text/plain"
+    return response
