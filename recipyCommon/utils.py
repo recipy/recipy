c41383cf5de539efaba19c042096770aecc10b53
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-03-23 10:32:12 +0100

    Issue warning if recipy is imported after patched modules
    Refs #108

--- recipyCommon/utils.py
+++ recipyCommon/utils.py
@@ -1,6 +1,8 @@
 import wrapt
 import imp
 import os
+import sys
+import warnings
 
 from tinydb import TinyDB
 from .tinydb_utils import serialization
@@ -16,8 +18,13 @@ def open_or_create_db(path=get_db_path()):
 
     return db
 
+
 def multiple_insert(lst, items):
     for item in items:
+        if item.modulename in sys.modules:
+            msg = 'unable to patch module; recipy was imported after {}'. \
+                format(item.modulename)
+            warnings.warn(msg, stacklevel=4)
         lst.insert(0, item)
 
 def recursive_getattr(obj, attr):
