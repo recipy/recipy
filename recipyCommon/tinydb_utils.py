2620bc3173653ef475d46a2843f82cc962ce4ee7
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-04-13 17:49:46 +0200

    Add advanced search function for inputs and outputs
    Add search function that either searches a list of filenames (if hashing
    is not enabled) or searches a list of lists [file name, hash].
    
    This function was added for backwards compatibility and the possibility
    to store runs with hashes enabled and runs with hashes not enabled in
    the same database.

--- recipyCommon/tinydb_utils.py
+++ recipyCommon/tinydb_utils.py
@@ -1,3 +1,5 @@
+import six
+import re
 from datetime import datetime
 
 from tinydb_serialization import Serializer, SerializationMiddleware
@@ -16,3 +18,28 @@ class DateTimeSerializer(Serializer):
 
 serializer = SerializationMiddleware()
 serializer.register_serializer(DateTimeSerializer(), 'TinyDate')
+
+
+def listsearch(query, item):
+    """Return match with query on an item from the list of input/output files
+
+    The lists of input and output files can consist either of file names
+    (strings), or lists containing the filename and the hash of the file (if
+    hashing is enabled). This function provides a transparent interface to
+    these two options.
+
+    Parameters:
+        query : str
+            The search query
+        item : str or list containing two strings
+            A file name or a list containing a file name and hash
+
+    Returns:
+        None or something that is True
+    """
+    fh = ''
+    if not isinstance(item, six.string_types):
+        item = item[0]
+        fh = item[1]
+
+    return re.match(".+%s.+" % query, item) or re.match(".+%s.+" % query, fh)
