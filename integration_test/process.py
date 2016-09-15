315c45e054d99913f6bbdf9e13617f08f6f430fb
mikej888 <michaelj@epcc.ed.ac.uk>
2016-09-15 16:18:27 +0100

    Added process.py with function to execute commands via the operating system

---
+++ integration_test/process.py
@@ -0,0 +1,32 @@
+"""
+Functions to execute commands via the operating system.
+"""
+
+from __future__ import (nested_scopes, generators, division,
+                        absolute_import, with_statement,
+                        print_function, unicode_literals)
+
+import subprocess
+
+
+def execute(command, arguments, stdout=None, stderr=None):
+    """
+    Run a command via the operating system.
+
+    :param command: Command to run
+    :type command: str or unicode
+    :param arguments: Arguments to the command
+    :type arguments: list of str or unicode
+    :param stdout: File for standard output stream
+    :type stdout: file
+    :param stderr: File for standard error stream
+    :type stderr: file
+    :return: exit code
+    :rtype: int
+    :raises OSError: if there are problems running the command
+    """
+    command_line = [command]
+    command_line.extend(arguments)
+    print((" ".join(command_line)))
+    return_code = subprocess.call(command_line, stdout=stdout, stderr=stderr)
+    return return_code
