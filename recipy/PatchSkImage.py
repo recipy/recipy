fe9d5a5e119437cbbc8c6826f14ba4b3483bc064
Robin Wilson <robin@rtwilson.com>
2016-09-19 16:01:41 +0100

    Skimage now working, PIL working mostly, plus changes to default arg vals for sanity

--- recipy/PatchSkImage.py
+++ recipy/PatchSkImage.py
@@ -3,3 +3,9 @@ from .PatchVerySimple import PatchVerySimple
 PatchSKImageIO = PatchVerySimple(modulename='skimage.io',
                                  input_functions=['imread', 'load_sift', 'load_surf'],
                                  output_functions=['imsave'])
+
+PatchPillow = PatchVerySimple(modulename='PIL.Image',
+                              input_functions=['open'])
+
+PatchPillow2 = PatchVerySimple(modulename='PIL.TiffImagePlugin',
+                               output_functions=['TiffImageFile.save'])
--- recipy/PatchVerySimple.py
+++ recipy/PatchVerySimple.py
@@ -3,24 +3,28 @@ import sys
 from .PatchSimple import PatchSimple
 
 from .log import log_input, log_output, add_module_to_db
-from recipyCommon.utils import create_wrapper, create_wrapper_simple, multiple_insert
+from recipyCommon.utils import create_wrapper_simple
+
 
 class PatchVerySimple(PatchSimple):
 
-    # input_wrapper = create_wrapper(log_input, 0, 'skimage.io')
-    # output_wrapper = create_wrapper(log_output, 0, 'skimage.io')
-
-    def __init__(self, modulename, input_functions, output_functions,
+    def __init__(self, modulename, input_functions=[], output_functions=[],
                  input_wrapper=None, output_wrapper=None):
         self.modulename = modulename
-        #print(modulename)
 
         self.input_functions = input_functions
 
         self.output_functions = output_functions
 
-        self.input_wrapper = create_wrapper_simple(log_input, 0, 'skimage.io')
-        self.output_wrapper = create_wrapper_simple(log_output, 0, 'skimage.io')
+        if input_wrapper is None:
+            self.input_wrapper = create_wrapper_simple(log_input, 0, modulename)
+        else:
+            self.input_wrapper = input_wrapper
+
+        if output_wrapper is None:
+            self.output_wrapper = create_wrapper_simple(log_output, 0, modulename)
+        else:
+            self.output_wrapper = output_wrapper
 
         add_module_to_db(modulename, input_functions, output_functions)
 
