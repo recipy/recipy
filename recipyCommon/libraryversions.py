8b4d59cf21ba89d39595bf5e43921eefa0d981ae
Robin Wilson <robin@rtwilson.com>
2016-09-19 14:11:00 +0100

    Use root module when trying to find version, so it works for `matplotlib.pyplot` etc

--- recipyCommon/libraryversions.py
+++ recipyCommon/libraryversions.py
@@ -5,6 +5,11 @@ import warnings
 def get_version(modulename):
     "Return a string containing the module name and the library version."
     version = '?'
+
+    # Get the root module name (in case we have something like `recipy.open`
+    # or `matplotlib.pyplot`)
+    modulename = modulename.split('.')[0]
+
     if modulename in sys.modules:
         try:
             version = sys.modules[modulename].__version__
