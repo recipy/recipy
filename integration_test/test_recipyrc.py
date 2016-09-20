6b312dae599e2d2eaf9e4597529515a87a805505
mikej888 <michaelj@epcc.ed.ac.uk>
2016-09-20 17:28:22 +0100

    Completed recipy configuration precedence tests

--- integration_test/test_recipyrc.py
+++ integration_test/test_recipyrc.py
@@ -1,36 +1,67 @@
+"""
+Tests of recipy configuration, provided via 'recipyrc' configuration files.
+"""
+
 import os
 import os.path
-from os.path import expanduser
 import shutil
 import tempfile
-
 import pytest
 
-from .database import open_db
 from .process import execute
+from .environment import get_recipy_dir, get_recipydb, get_recipyrc
 
-test_files = []
+test_files = []  # pylint: disable=C0103
+test_script = "run_numpy.py"  # pylint: disable=C0103
+test_data = "run_numpy.dat"  # pylint: disable=C0103
 
-def get_home():
-    return expanduser("~")
-def get_recipy_dir():
-    return os.path.expanduser("~/.recipy/")
-def get_recipydb():
-    return os.path.join(get_recipy_dir(), "recipyDB.json")
-def get_recipyrc():
-    return os.path.join(get_recipy_dir(), "recipyrc")
+
 def run_script():
-    execute("python", ["run_numpy.py"])
-
-# TODO add to environment.py
-# CONFIG.read(os.path.expanduser("~/.recipy/recipyrc"))
-# return os.path.expanduser('~/.recipy/recipyDB.json')
+    execute("python", [test_script])
 
 
-def create_database_config(recipyrc, recipydb):
-    with open(recipyrc, 'w') as f:
+def append_recipyrc_database(recipyrc, recipydb):
+    with open(recipyrc, 'a') as f:
         f.write('[database]\n')
         f.write('path=' + recipydb + '\n')
+
+
+def setup_module(module):
+    """
+    py.test-compliant setup function, run when module is loaded,
+    creates TMP/run_numpy.py.
+
+    :param functon: Test function
+    :type function: function
+    """
+    global test_script, test_data
+    test_script = os.path.join(tempfile.gettempdir(), "run_numpy.py")
+    test_data = os.path.join(tempfile.gettempdir(), "run_numpy.dat")
+    with open(test_script, "w") as f:
+        f.write("import recipy")
+        f.write("\n")
+        f.write("import numpy as np")
+        f.write("\n")
+        f.write("data = np.array([list(range(4,8))])")
+        f.write("\n")
+        f.write("np.savetxt(" +
+                str(test_data).__repr__() +
+                ", data, delimiter=',')")
+        f.write("\n")
+
+
+def teardown_module(module):
+    """
+    py.test-compliant setup function, run when module is completed,
+    deletes TMP/run_numpy.py and TMP/run_numpy.dat.
+
+    :param functon: Test function
+    :type function: function
+    """
+    if os.path.isfile(test_script):
+        os.remove(test_script)
+    if os.path.isfile(test_data):
+        os.remove(test_data)
 
 
 def setup_function(function):
@@ -42,10 +73,8 @@ def setup_function(function):
     :type function: function
     """
     global test_files
-    print("\nSETUP")
-    print(type(function))
     recipy_dir = get_recipy_dir()
-    if (os.path.isdir(recipy_dir)):
+    if os.path.isdir(recipy_dir):
         shutil.rmtree(recipy_dir)
     os.mkdir(recipy_dir)
     test_files = []
@@ -59,11 +88,8 @@ def teardown_function(function):
     :param functon: Test function
     :type function: function
     """
-    global test_files
-    print("\nTEARDOWN")
     for test_file in test_files:
         if os.path.isfile(test_file):
-            print("Removing", test_file)
             os.remove(test_file)
 
 
@@ -72,85 +98,66 @@ def test_default():
     If neither .recipyrc, recipyrc nor ~/recipyrc exist then test that
     recipy uses its default configuration.
     """
-    global test_files
     recipydb = get_recipydb()
     test_files.append(recipydb)
     run_script()
     assert os.path.isfile(recipydb), ("Expected to find " + recipydb)
 
 
-def test_local_recipydb():
+@pytest.mark.parametrize("recipyrc", [
+    get_recipyrc(),
+    os.path.join(os.getcwd(), ".recipyrc"),
+    os.path.join(os.getcwd(), "recipyrc")])
+def test_local_recipyrc(recipyrc):
     """
-    If ~/recipy/recipyrc is present. then test that its configuration
-    is used.
-    """
-    global test_files
-    recipydb =  os.path.join(os.getcwd(), "recipyDB.json")
-    test_files.append(recipydb)
-    recipyrc = get_recipyrc()
-    create_database_config(recipyrc, recipydb)
-    run_script()
-    assert os.path.isfile(recipydb), ("Expected to find " + recipydb)
+    If ~/recipy/recipyrc, .recipy or recipyrc are present. then test
+    that their configuration is used.
 
-
-@pytest.mark.parametrize("config_file", [".recipyrc", "recipyrc"])
-def test_local_recipyrc(config_file):
+    :param recipyrc: recipyrc file
+    :type recipyrc: str or unicode
     """
-    If .recipy or recipyrc are present. then test that their
-    configuration is used.
-
-    :param config_file: recipyrc file
-    :type config_file: str or unicode
-    """
-    global test_files
-    recipyrc = os.path.join(os.getcwd(), config_file)
     test_files.append(recipyrc)
     recipydb = os.path.join(os.getcwd(), "recipyDB.json")
     test_files.append(recipydb)
-    create_database_config(recipyrc, recipydb)
+    append_recipyrc_database(recipyrc, recipydb)
     run_script()
     assert os.path.isfile(recipydb), ("Expected to find " + recipydb)
-    assert len(os.listdir(get_recipy_dir())) == 0, "Expected .recipy to be empty"
 
 
-@pytest.mark.parametrize("useit",[
-     (".recipyrc", get_recipyrc()),
-     ("recipyrc", get_recipyrc()),
-     ("recipyrc", os.path.join(os.getcwd(), ".recipyrc"))
-    ])
-def test_local_recipyrc_precedence(useit):
+@pytest.mark.parametrize("recipyrc_files", [
+    (os.path.join(os.getcwd(), ".recipyrc"), get_recipyrc()),
+    (os.path.join(os.getcwd(), "recipyrc"), get_recipyrc()),
+    (os.path.join(os.getcwd(), "recipyrc"),
+     os.path.join(os.getcwd(), ".recipyrc"))])
+def test_local_recipyrc_precedence(recipyrc_files):
     """
     Test the following scenarios:
+
     * If both .recipyrc and ~/recipy/recipyrc are present then the
       former's configuration is used.
     * If both recipyrc and ~/recipy/recipyrc are present then the
-      former's configuration is used. 
+      former's configuration is used.
     * If both recipyrc and .recipyrc are present then the former's
-      configuration is used.  
-    """
-    global test_files
-    (config_file, ignore_recipyrc) = useit
+      configuration is used.
 
-    recipyrc = os.path.join(os.getcwd(), config_file)
+    :param recipyrc_files: two recipyrc files, the former of which is
+     expected to take precedence
+    :type recipyrc_files: tuple of (str or unicode, str or unicode)
+    """
+    (recipyrc, ignore_recipyrc) = recipyrc_files
+
     test_files.append(recipyrc)
+    test_files.append(ignore_recipyrc)
+
     recipydb = os.path.join(os.getcwd(), "recipyDB.json")
     test_files.append(recipydb)
-    create_database_config(recipyrc, recipydb)
+    append_recipyrc_database(recipyrc, recipydb)
 
-    test_files.append(ignore_recipyrc)
     ignore_recipydb = os.path.join(os.getcwd(), "ignoreDB.json")
     test_files.append(ignore_recipydb)
-    create_database_config(ignore_recipyrc, ignore_recipydb)
+    append_recipyrc_database(ignore_recipyrc, ignore_recipydb)
 
     run_script()
     assert os.path.isfile(recipydb), ("Expected to find " + recipydb)
-    assert not os.path.isfile(ignore_recipydb), ("Did not expect to find " + ignore_recipydb)
-
-
-
-# import recipy
-# import numpy as np
-# data = np.array([list(range(4,8)), list(range(12,16))])
-# np.savetxt("tmp.csv", data, delimiter=",")
-# np.savetxt("tmp.txt", data, delimiter=",")
-# np.loadtxt("tmp.csv", delimiter=",")
+    assert not os.path.isfile(ignore_recipydb),\
+        ("Did not expect to find " + ignore_recipydb)
