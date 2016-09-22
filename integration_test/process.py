0dcc1700849121de15fa68747a817ac4ccf399b6
mikej888 <michaelj@epcc.ed.ac.uk>
2016-09-22 09:22:24 +0100

    Added execute_and_capture

--- integration_test/process.py
+++ integration_test/process.py
@@ -8,7 +8,7 @@ from __future__ import (nested_scopes, generators, division,
                         absolute_import, with_statement,
                         print_function, unicode_literals)
 
-import subprocess
+from subprocess import call, check_output, CalledProcessError, STDOUT
 
 
 def execute(command, arguments, stdout=None, stderr=None):
@@ -30,5 +30,28 @@ def execute(command, arguments, stdout=None, stderr=None):
     command_line = [command]
     command_line.extend(arguments)
     print((" ".join(command_line)))
-    return_code = subprocess.call(command_line, stdout=stdout, stderr=stderr)
+    return_code = call(command_line, stdout=stdout, stderr=stderr)
     return return_code
+
+
+def execute_and_capture(command, arguments):
+    """
+    Run a command via the operating system and capture and return
+    standard output and standard error.
+
+    :param command: Command to run
+    :type command: str or unicode
+    :param arguments: Arguments to the command
+    :type arguments: list of str or unicode
+    :return: (exit code, standard output and error)
+    :rtype: (int, str or unicode)
+    :raises OSError: if there are problems running the command
+    """
+    command_line = [command]
+    command_line.extend(arguments)
+    print((" ".join(command_line)))
+    try:
+        result = check_output(command_line, stderr=STDOUT)
+        return (0, str(result))
+    except CalledProcessError as error:
+        return (error.returncode, str(error.output))
