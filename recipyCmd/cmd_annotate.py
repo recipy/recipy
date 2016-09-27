ea522adad5e08a507c80af67fa66482d8a52132b
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-09-28 00:57:30 +0100

    refactor annotate

--- recipyCmd/cmd_annotate.py
+++ recipyCmd/cmd_annotate.py
@@ -1,8 +1,5 @@
-import os
 import click
 from tinydb import where
-import tempfile
-from recipyCommon.config import get_editor
 from recipyCmd.templating import render_run_template
 from recipyCmd.recipycmd import db, get_latest_run
 
@@ -16,7 +13,6 @@ def cmd(id):
     \b
     For example: recipy tag -id 67d8
     Would add a note to run which id starts with 67d8"""
-    editor = get_editor()
 
     if id:
         results = db.search(where('unique_id').matches('{}.*'.format(id)))
@@ -30,38 +26,22 @@ def cmd(id):
     else:
         run = get_latest_run()
 
-    # Get temp filename
-    f = tempfile.NamedTemporaryFile(delete=False, mode='w')
+    notes = get_message(run)
 
-    if run.get('notes'):
-        f.write(run['notes'])
-
-    # Write something to the bottom of it
-    f.write('\n' + '-' * 80 + '\n')
-    f.write('\n')
-    f.write('Enter your notes on this run above this line')
-    f.write('\n' * 3)
-    f.write(render_run_template(run, nocolor=True))
-
-    f.close()
-
-    # Open your editor
-    os.system('%s %s' % (editor, f.name))
-
-    # Grab the text
-    annotation = ""
-    with open(f.name, 'r') as f:
-        for line in f:
-            if line == '-' * 80 + '\n':
-                break
-            annotation += line
-
-    notes = annotation.strip()
-
-    if notes == "":
-        print('No annotation entered, exiting.')
+    if not notes:
+        print('No notes added to run {}'.format(run['unique_id']))
         return
 
-    # Store in the DB
     db.update({'notes': notes}, where('unique_id') == run['unique_id'])
     db.close()
+
+
+def get_message(run):
+    """Gets message from user using default text editor."""
+    notes = run.get('notes', '') + '\n'
+    marker = '-' * 70
+    instructions = 'Enter your notes above this line.\n'
+    run_tmpl = render_run_template(run, nocolor=True)
+    message = click.edit('\n'.join([notes, marker, instructions, run_tmpl]))
+    if message is not None:
+        return message.split(marker, 1)[0].rstrip('\n')
