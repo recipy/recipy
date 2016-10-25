408e53119e63dd1c988d5721303efa3e30c342d2
mikej888 <michaelj@epcc.ed.ac.uk>
2016-10-25 10:51:45 +0100

    Removed TestCaseRunner and pulled methods up to be module-level functions, as no state was retained

--- integration_test/test_packages.py
+++ integration_test/test_packages.py
@@ -296,199 +296,192 @@ def get_test_case_function_name(script_test_case):
     return function_name
 
 
-class TestCaseRunner(object):
+def run_test_case(script_path, command, test_case):
     """
-    recipy test case runner.
+    Run a single test case. This runs a script using arguments in
+    test_case and validates that recipy has logged information
+    about the script, also using data in test_case.
+
+    test_case is assumed to have the following
+    entries:
+
+    * 'libraries': a list of one or more libraries e.g. ['numpy'].
+    * 'arguments': a list of script arguments e.g. ['loadtxt'],
+    ['savetxt']. If none, then this can be omitted.
+    * 'inputs': a list of zero or more input files which running
+      the script with the argument will read e.g. ['data.csv']. If
+      none, then this can be omitted.
+    * 'outputs': a list of zero or more output files which running
+      the script with the argument will write
+      e.g. ['data.csv']. If none, then this can be omitted.
+
+    :param script_path: Path to the script.
+    :type script_path: str or unicode
+    :param commmand: Command-line invocation used to run the script
+    (not including "python" or any arguments, which are test-case
+    specific).
+    :type command: str or unicode
+    :param test_case: Test case configuration.
+    :type test_case: dict
     """
-
-    def run_test_case(self, script_path, command, test_case):
-        """
-        Run a single test case. This runs a script using arguments in
-        test_case and validates that recipy has logged information
-        about the script, also using data in test_case.
-
-        test_case is assumed to have the following
-        entries:
-
-        * 'libraries': a list of one or more libraries
-          e.g. ['numpy'].
-        * 'arguments': a list of script arguments e.g. ['loadtxt'],
-          ['savetxt']. If none, then this can be omitted.
-        * 'inputs': a list of zero or more input files which running
-          the script with the argument will read e.g. ['data.csv']. If
-          none, then this can be omitted.
-        * 'outputs': a list of zero or more output files which running
-          the script with the argument will write
-          e.g. ['data.csv']. If none, then this can be omitted.
-
-        :param script_path: Path to the script.
-        :type script_path: str or unicode
-        :param commmand: Command-line invocation used to run the
-        script (not including "python" or any arguments, which are
-        test-case specific).
-        :type command: str or unicode
-        :param test_case: Test case configuration.
-        :type test_case: dict
-        """
-        number_of_logs = 0
-        try:
-            number_of_logs =\
-               helpers.get_number_of_logs(recipyenv.get_recipydb())
-        except DatabaseError:
-            # Database may not exist if running tests for first time so
-            # give benefit of doubt at this stage and assume running script
-            # will bring it into life.
-            pass
-        libraries = test_case[LIBRARIES]
-        if ARGUMENTS in test_case:
-            arguments = test_case[ARGUMENTS]
-        else:
-            arguments = []
-        # Execute script
-        _, _ = helpers.execute_python(command + arguments, 0)
-        # Validate recipy database
-        log, _ = helpers.get_log(recipyenv.get_recipydb())
-        # Number of logs
-        new_number_of_logs =\
+    number_of_logs = 0
+    try:
+        number_of_logs =\
             helpers.get_number_of_logs(recipyenv.get_recipydb())
-        assert new_number_of_logs == (number_of_logs + 1),\
-            ("Unexpected number of logs " + new_number_of_logs)
-        # Script that was invoked
-        self.check_script(script_path, log["script"],
-                          arguments, log["command_args"])
-        # Libraries
-        self.check_libraries(libraries, log["libraries"])
-        # Inputs and outputs (local filenames only)
-        self.check_input_outputs(test_case,
-                                 INPUTS,
-                                 log["inputs"])
-        self.check_input_outputs(test_case,
-                                 OUTPUTS,
-                                 log["outputs"])
-        # Dates
-        self.check_dates(log["date"], log["exit_date"])
-        # Execution environment
-        self.check_environment(log["command"], log["environment"])
-        # Miscellaneous
-        assert environment.get_user() == log["author"], "Unexpected author"
-        assert log["description"] == "", "Unexpected description"
-        assert [] == log["warnings"], "Unexpected warnings"
+    except DatabaseError:
+        # Database may not exist if running tests for first time so
+        # give benefit of doubt at this stage and assume running script
+        # will bring it into life.
+        pass
+    libraries = test_case[LIBRARIES]
+    if ARGUMENTS in test_case:
+        arguments = test_case[ARGUMENTS]
+    else:
+        arguments = []
+    # Execute script
+    _, _ = helpers.execute_python(command + arguments, 0)
+    # Validate recipy database
+    log, _ = helpers.get_log(recipyenv.get_recipydb())
+    # Number of logs
+    new_number_of_logs =\
+        helpers.get_number_of_logs(recipyenv.get_recipydb())
+    assert new_number_of_logs == (number_of_logs + 1),\
+        ("Unexpected number of logs " + new_number_of_logs)
+    # Script that was invoked
+    check_script(script_path, log["script"],
+                 arguments, log["command_args"])
+    # Libraries
+    check_libraries(libraries, log["libraries"])
+    # Inputs and outputs (local filenames only)
+    check_input_outputs(test_case, INPUTS, log["inputs"])
+    check_input_outputs(test_case, OUTPUTS, log["outputs"])
+    # Dates
+    check_dates(log["date"], log["exit_date"])
+    # Execution environment
+    check_environment(log["command"], log["environment"])
+    # Miscellaneous
+    assert environment.get_user() == log["author"], "Unexpected author"
+    assert log["description"] == "", "Unexpected description"
+    assert [] == log["warnings"], "Unexpected warnings"
 
-    def check_script(self, script, logged_script,
-                     arguments, logged_arguments):
-        """
-        Check script and arguments logged by recipy.
 
-        :param script: Script specified in test configuration
-        :type script: str or unicode
-        :param logged_script: Script logged by recipy
-        :type logged_script: str or unicode
-        :param arguments: Arguments specified in test configuration
-        :type arguments: list
-        :param logged_arguments: Arguments logged by recipy
-        :type logged_arguments: list
-        """
-        # Rather than simple equality check, see if paths refer to the
-        # same file. Avoids problems with forward vs backslashes and
-        # inconsistent paths on Anaconda Python on Windows, for
-        # example.
-        assert os.path.samefile(script, logged_script)
-        assert " ".join(arguments) == logged_arguments,\
-               "Unexpected command_args"
+def check_script(script, logged_script, arguments, logged_arguments):
+    """
+    Check script and arguments logged by recipy.
 
-    def check_libraries(self, libraries, logged_libraries):
-        """
-        Check libraries logged by recipy.
+    :param script: Script specified in test configuration
+    :type script: str or unicode
+    :param logged_script: Script logged by recipy
+    :type logged_script: str or unicode
+    :param arguments: Arguments specified in test configuration
+    :type arguments: list
+    :param logged_arguments: Arguments logged by recipy
+    :type logged_arguments: list
+    """
+    # Rather than simple equality check, see if paths refer to the
+    # same file. Avoids problems with forward vs backslashes and
+    # inconsistent paths on Anaconda Python on Windows, for
+    # example.
+    assert os.path.samefile(script, logged_script)
+    assert " ".join(arguments) == logged_arguments, "Unexpected command_args"
 
-        :param libraries: Libraries specified in test configuration
-        :type libraries: list of str or unicode
-        :param logged_libraries: Libraries logged by recipy
-        :type logged_libraries: list of str or unicode
-        :raises ConfigError: if any library is not installed
-        """
-        packages = environment.get_packages()
-        for library in libraries:
-            if environment.is_package_installed(packages, library):
-                version = environment.get_package_version(packages, library)
-                library_version = library + " v" + version
-                assert library_version in logged_libraries,\
-                    ("Could not find library " + library_version)
-            else:
-                raise ConfigError((
-                    "Library {} is not installed".format(library)))
 
-    def check_dates(self, logged_start_date, logged_end_date):
-        """
-        Check dates logged by recipy.
+def check_libraries(libraries, logged_libraries):
+    """
+    Check libraries logged by recipy.
 
-        :param logged_start_date: Start date logged by recipy
-        :type logged_start_date: str or unicode
-        :param logged_end_date: End date logged by recipy
-        :type logged_end_date: str or unicode
-        """
-        try:
-            start_date = environment.get_tinydatestr_as_date(logged_start_date)
-        except ValueError as _:
-            assert False, "date is not a valid date string"
-        try:
-            exit_date = environment.get_tinydatestr_as_date(logged_end_date)
-        except ValueError as _:
-            assert False, "end_date is not a valid date string"
-        assert start_date <= exit_date, "date is not before exit_date"
-
-    def check_environment(self, logged_command, logged_environment):
-        """
-        Check environment logged by recipy.
-
-        :param logged_command: Python executable logged by recipy
-        :type logged_command: str or unicode
-        :param logged_environment: Operating system and Python
-        version logged by recipy
-        :type logged_environment: list of str or unicore
-        """
-        assert environment.get_python_exe() == logged_command,\
-            "Unexpected command"
-        assert environment.get_os() in logged_environment,\
-            "Cannot find operating system in environment"
-        python_version = "python " + environment.get_python_version()
-        assert python_version in logged_environment,\
-            "Cannot find Python in environment"
-
-    def check_input_outputs(self, test_case, io_key, logged_io):
-        """
-        Check inputs/outputs logged by recipy.
-
-        :param test_case: Test case configuration
-        :type test_case: dict
-        :param io_key: "inputs" or "outputs", key into test_case
-        :type io_key: str or unicode
-        :param logged_io: Inputs/outputs logged by recipy
-        :type logged_io: list
-        """
-        if io_key in test_case:
-            io_files = test_case[io_key]
+    :param libraries: Libraries specified in test configuration
+    :type libraries: list of str or unicode
+    :param logged_libraries: Libraries logged by recipy
+    :type logged_libraries: list of str or unicode
+    :raises ConfigError: if any library is not installed
+    """
+    packages = environment.get_packages()
+    for library in libraries:
+        if environment.is_package_installed(packages, library):
+            version = environment.get_package_version(packages, library)
+            library_version = library + " v" + version
+            assert library_version in logged_libraries,\
+                ("Could not find library " + library_version)
         else:
-            io_files = []
-        assert len(io_files) == len(logged_io),\
-            ("Unexpected number of " + io_key)
-        # Convert logged files to local file names.
-        logged_files = [os.path.basename(file_name)
-                        for [file_name, _] in logged_io]
-        for io_file in io_files:
-            assert io_file in logged_files,\
-                ("Could not find " + io_key + " " + io_file)
+            raise ConfigError(("Library {} is not installed".format(library)))
 
-    @pytest.mark.parametrize("script_test_case",
-                             get_test_cases(),
-                             ids=get_test_case_function_name)
-    def test_scripts(self, script_test_case):
-        """
-        Run a test defined in the recipy test configuration.
 
-        :param script_test_case: Ncript path, command, test case
-        specification - consistent with a tuple from
-        get_script_test_cases.
-        :type script_test_case: (str or unicode, str or unicode, dict)
-        """
-        (script_path, command, test_case) = script_test_case
-        self.run_test_case(script_path, command, test_case)
+def check_dates(logged_start_date, logged_end_date):
+    """
+    Check dates logged by recipy.
+
+    :param logged_start_date: Start date logged by recipy
+    :type logged_start_date: str or unicode
+    :param logged_end_date: End date logged by recipy
+    :type logged_end_date: str or unicode
+    """
+    try:
+        start_date = environment.get_tinydatestr_as_date(logged_start_date)
+    except ValueError as _:
+        assert False, "date is not a valid date string"
+    try:
+        exit_date = environment.get_tinydatestr_as_date(logged_end_date)
+    except ValueError as _:
+        assert False, "end_date is not a valid date string"
+    assert start_date <= exit_date, "date is not before exit_date"
+
+
+def check_environment(logged_command, logged_environment):
+    """
+    Check environment logged by recipy.
+
+    :param logged_command: Python executable logged by recipy
+    :type logged_command: str or unicode
+    :param logged_environment: Operating system and Python
+    version logged by recipy
+    :type logged_environment: list of str or unicore
+    """
+    assert environment.get_python_exe() == logged_command,\
+        "Unexpected command"
+    assert environment.get_os() in logged_environment,\
+        "Cannot find operating system in environment"
+    python_version = "python " + environment.get_python_version()
+    assert python_version in logged_environment,\
+        "Cannot find Python in environment"
+
+
+def check_input_outputs(test_case, io_key, logged_io):
+    """
+    Check inputs/outputs logged by recipy.
+
+    :param test_case: Test case configuration
+    :type test_case: dict
+    :param io_key: "inputs" or "outputs", key into test_case
+    :type io_key: str or unicode
+    :param logged_io: Inputs/outputs logged by recipy
+    :type logged_io: list
+    """
+    if io_key in test_case:
+        io_files = test_case[io_key]
+    else:
+        io_files = []
+    assert len(io_files) == len(logged_io),\
+        ("Unexpected number of " + io_key)
+    # Convert logged files to local file names.
+    logged_files = [os.path.basename(file_name)
+                    for [file_name, _] in logged_io]
+    for io_file in io_files:
+        assert io_file in logged_files,\
+               ("Could not find " + io_key + " " + io_file)
+
+
+@pytest.mark.parametrize("script_test_case",
+                         get_test_cases(),
+                         ids=get_test_case_function_name)
+def test_scripts(script_test_case):
+    """
+    Run a test defined in the recipy test configuration.
+
+    :param script_test_case: Ncript path, command, test case
+    specification - consistent with a tuple from
+    get_script_test_cases.
+    :type script_test_case: (str or unicode, str or unicode, dict)
+    """
+    (script_path, command, test_case) = script_test_case
+    run_test_case(script_path, command, test_case)
