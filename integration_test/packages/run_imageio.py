b9d8bbabec3a173cff5f5c85c057cd843d768404
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2018-07-25 14:26:05 +0200

    Refactor imageio and tifffile tests
    These modules are now tested with the recipy testing framework.

---
+++ integration_test/packages/run_imageio.py
@@ -0,0 +1,57 @@
+"""
+Sample script that runs lxml functions logged by recipy.
+"""
+
+# Copyright (c) 2016 University of Edinburgh.
+
+from __future__ import (nested_scopes, generators, division,
+                        absolute_import, with_statement,
+                        print_function, unicode_literals)
+import recipy
+
+import os
+import sys
+import imageio
+
+from integration_test.packages.base import Base
+
+
+class ImageioSample(Base):
+    """
+    Sample script that runs imageio functions logged by recipy.
+
+    This class assumes the existence of a data/imageio directory,
+    co-located with this file, with the following content:
+
+    * image.tiff: tiff file
+    """
+
+    def __init__(self):
+        """
+        Constructor. Set data_dir attribute with path to data files needed
+        by this class.
+        """
+        Base.__init__(self)
+        self.data_dir = os.path.join(self.current_dir, "data", "imageio")
+
+    def imread(self):
+        """
+        Use imageio.imread to read image.tiff.
+        """
+        file_name = os.path.join(self.data_dir, "image.tiff")
+        imageio.imread(file_name)
+
+    def imwrite(self):
+        """
+        Use imageio.imwrite to write image.png.
+        """
+        file_name_in = os.path.join(self.data_dir, "image.tiff")
+        file_name = os.path.join(self.data_dir, "image.png")
+        im = imageio.imread(file_name_in)
+
+        imageio.imwrite(file_name, im)
+        os.remove(file_name)
+
+
+if __name__ == "__main__":
+    ImageioSample().invoke(sys.argv)
---
+++ integration_test/packages/run_tifffile.py
@@ -0,0 +1,60 @@
+"""
+Sample script that runs lxml functions logged by recipy.
+"""
+
+# Copyright (c) 2016 University of Edinburgh.
+
+from __future__ import (nested_scopes, generators, division,
+                        absolute_import, with_statement,
+                        print_function, unicode_literals)
+import recipy
+
+import os
+import sys
+import numpy
+import tifffile
+
+from integration_test.packages.base import Base
+
+
+class TifffileSample(Base):
+    """
+    Sample script that runs tifffile functions logged by recipy.
+
+    This class assumes the existence of a data/tifffile directory,
+    co-located with this file, with the following content:
+
+    * image.tiff: tiff file
+    """
+
+    def __init__(self):
+        """
+        Constructor. Set data_dir attribute with path to data files needed
+        by this class.
+        """
+        Base.__init__(self)
+        self.data_dir = os.path.join(self.current_dir, "data", "tifffile")
+
+    def imread(self):
+        """
+        Use tifffile.imread to read image.tiff.
+        """
+        file_name = os.path.join(self.data_dir, "image.tiff")
+        tifffile.imread(file_name)
+
+    def imsave(self):
+        """
+        Use tifffile.imsave to write image2.tiff.
+        """
+        #file_name_in = os.path.join(self.data_dir, "image.tiff")
+        file_name = os.path.join(self.data_dir, "image2.tiff")
+
+        data = numpy.array([1, 2, 3])
+        #im = tifffile.imread(file_name_in)
+
+        tifffile.imsave(file_name, data)
+        os.remove(file_name)
+
+
+if __name__ == "__main__":
+    TifffileSample().invoke(sys.argv)
