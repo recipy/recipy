43c56d2c5baf4269803417d0c9a5878635cd1c74
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2018-07-25 14:54:25 +0200

    Refactor finding the version of installed libraries
    Fixes the v? problem for lxml. Probably also for gdal. (I was
    unable to install gdal locally, so I haven't tested that.)

--- recipyCommon/libraryversions.py
+++ recipyCommon/libraryversions.py
@@ -1,4 +1,5 @@
 import sys
+import pkg_resources
 import warnings
 import numbers
 
@@ -14,32 +15,9 @@ def get_version(modulename):
     modulename = modulename.split('.')[0]
 
     if modulename in sys.modules:
-        mod = sys.modules[modulename]
-
-        try:
-            version = mod.__version__
-        except (AttributeError, TypeError, KeyError):
-            pass
-
-        try:
-            version = mod.version
-        except (AttributeError, TypeError, KeyError):
-            pass
-
-        try:
-            version = mod.version.version
-        except (AttributeError, TypeError, KeyError):
-            pass
-
-        try:
-            version = mod.VERSION
-        except (AttributeError, TypeError, KeyError):
-            pass
-
-        try:
-            version = mod.version()
-        except (AttributeError, TypeError, KeyError):
-            pass
+        ws = pkg_resources.working_set
+        package = ws.find(pkg_resources.Requirement(modulename))
+        version = package.version
     else:
         warnings.warn('requesting version of a module that has not been '
                       'imported ({})'.format(modulename))
