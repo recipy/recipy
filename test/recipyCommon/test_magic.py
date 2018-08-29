f3ab735ee447077ded1bdc0c4b928740aaf2419e
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2018-08-29 13:55:40 +0200

    Add tests
    - test if recipy was imported when the extension was loaded
    - test if no run id is created after running recipyOn immediately after
    the extension was loaded

--- test/recipyCommon/test_magic.py
+++ test/recipyCommon/test_magic.py
@@ -1,9 +1,29 @@
 import pytest
-import os
+import sys
+
+from IPython.utils.io import capture_output
 
 # For some reason numpy is already in sys.modules of the interactive shell
 # fixture. This means numpy won't be patched, and therefore, can't be used for
 # testing the magic. In an actual notebook, numpy can be patched.
+
+
+def test_recipy_imported_during_loading_of_extension(notebook):
+    assert 'recipy' in sys.modules
+
+
+def test_no_runid_after_recipyon_immediately_after_loading_extension(notebook):
+    with capture_output() as output:
+        notebook.run_line_magic(magic_name='recipyOn', line='')
+    assert 'recipy run inserted, with ID' not in output.stdout
+
+    notebook.run_line_magic(magic_name='recipyOff', line='')
+
+    with capture_output() as output:
+        notebook.run_line_magic(magic_name='recipyOn', line='')
+    assert 'recipy run inserted, with ID' in output.stdout
+
+    notebook.run_line_magic(magic_name='recipyOff', line='')
 
 
 def test_magic_recipyOff_without_recipyOn(notebook):
