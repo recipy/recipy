9eb6a8eda1e3aca558c39d3d2eee123645aa85dd
Olivia <Olivia.wilson@os.uk>
2018-10-18 15:56:41 +0100

    Added script to create sample data

--- integration_test/packages/run_keras.py
+++ integration_test/packages/run_keras.py
@@ -1,40 +1,48 @@
 """
-Sample script that runs netcdf4 functions logged by recipy.
+Sample script that runs sklearn functions logged by recipy.
 """
-
-# Copyright (c) 2016, 2018 University of Edinburgh and Netherlands eScience
-# Center
 
 from __future__ import (nested_scopes, generators, division,
                         absolute_import, with_statement,
                         print_function, unicode_literals)
+
 import recipy
 
 import os
 import sys
-import xarray
 import numpy as np
+import keras
+import imageio
 
 from integration_test.packages.base import Base
 
 
-class XarraySample(Base):
+class KerasSample(Base):
     """
-    Sample script that runs xarray functions logged by recipy.
+    Sample script that runs keras functions logged by recipy.
 
-    This class assumes the existence of a data/xarray directory,
+    This class assumes the existence of a data/keras directory,
     co-located with this file, with the following content:
 
-    * image.tiff
-    * soilPropertiesRhineMeuse30min.nc: test netcdf file (taken with permission
-      from https://github.com/UU-Hydro/PCR-GLOBWB_input_example, see
-      https://www.geosci-model-dev.net/11/2429/2018/gmd-11-2429-2018.html for
-      more information about this data)
-    * topoPropertiesRhineMeuse30min.nc: test netcdf file (taken with permission
-      from https://github.com/UU-Hydro/PCR-GLOBWB_input_example, see
-      https://www.geosci-model-dev.net/11/2429/2018/gmd-11-2429-2018.html for
-      more information about this data)
-    * data_array.nc: netcdf file containing a dataarray (instead of a dataset)
+    * mnist01.jpg
+    * mnist02.jpg
+    * mnist03.jpg
+    * mnist04.jpg
+    * mnist05.jpg
+    * mnist06.jpg
+    * mnist07.jpg
+    * mnist08.jpg
+    * mnist09.jpg
+    * mnist10.jpg
+    * mnist.jbl (use X_train, y_train = joblib.load('mnist.jbl'))
+    * Model.hdf5
+
+
+    Running this script with argument 'create_sample_data' will create
+    these files.
+
+    All functions that save files delete the files after saving,
+    to keep the directory clean.
     """
 
     def __init__(self):
@@ -43,84 +51,66 @@ class XarraySample(Base):
         by this class.
         """
         Base.__init__(self)
-        self.data_dir = os.path.join(self.current_dir, "data", "xarray")
+        self.data_dir = os.path.join(self.current_dir, "data", "keras")
 
-    def open_dataset(self):
+    def load_svmlight_file(self):
         """
-        Use xarray.open_dataset to read netcdf file.
+        Use sklearn.datasets.load_svmlight_file to load data.svmlight.
         """
-        file_name = os.path.join(self.data_dir,
-                                 "soilPropertiesRhineMeuse30min.nc")
-        xarray.open_dataset(file_name)
+        file_name = os.path.join(self.data_dir, "data.svmlight")
+        datasets.load_svmlight_file(file_name)
 
-    def open_mfdataset_glob(self):
+    def dump_svmlight_file(self):
         """
-        Use xarray.open_mfdataset to read multiple netcdf files with a glob
-        pattern.
+        Use sklearn.datasets.dump_svmlight_file to save out.svmlight.
         """
-        pattern = os.path.join(self.data_dir, "*PropertiesRhineMeuse30min.nc")
-        xarray.open_mfdataset(pattern)
-
-    def open_mfdataset_list(self):
-        """
-        Use xarray.open_mfdataset to read multiple netcdf files from a list.
-        """
-        file_names = [os.path.join(self.data_dir, f)
-                      for f in ('soilPropertiesRhineMeuse30min.nc',
-                                'topoPropertiesRhineMeuse30min.nc')]
-        xarray.open_mfdataset(file_names)
-
-    def open_rasterio(self):
-        """
-        Use xarray.open_rasterio to read image.tiff.
-        """
-        file_name = os.path.join(self.data_dir, "image.tiff")
-        xarray.open_rasterio(file_name)
-
-    def open_dataarray(self):
-        """
-        Use xarray.open_dataarray to read a netcdf file.
-        """
-        file_name = os.path.join(self.data_dir, "data_array.nc")
-        xarray.open_dataarray(file_name)
-
-    def dataset_to_netcdf(self):
-        """
-        Use xarray.Dataset.to_netcdf to write a netcdf file.
-        """
-        data = xarray.DataArray(np.random.randn(2, 3))
-        ds = xarray.Dataset({'foo': data, 'bar': ('x', [1, 2]), 'baz': np.pi})
-        file_name = os.path.join(self.data_dir, "data.nc")
-        ds.to_netcdf(file_name)
+        x = np.array([list(range(0, 5)), list(range(5, 10))])
+        y = np.array([10, 20])
+        file_name = os.path.join(self.data_dir, "out.svmlight")
+        datasets.dump_svmlight_file(x, y, file_name,
+                                    comment="Sample svmlight file")
         os.remove(file_name)
 
-    def dataarray_to_netcdf(self):
+    def create_sample_data(self):
         """
-        Use xarray.DataArray.to_netcdf to write a netcdf file.
+        Create sample data files. The files created are:
+
+            * mnist01.jpg
+            * mnist02.jpg
+            * mnist03.jpg
+            * mnist04.jpg
+            * mnist05.jpg
+            * mnist06.jpg
+            * mnist07.jpg
+            * mnist08.jpg
+            * mnist09.jpg
+            * mnist10.jpg
+            * mnist.jbl (use X_train, y_train = joblib.load('mnist.jbl'))
         """
-        data = xarray.DataArray(np.random.randn(2, 3))
-        file_name = os.path.join(self.data_dir, "data.nc")
-        data.to_netcdf(file_name)
-        os.remove(file_name)
+        (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
 
-    def save_mfdataset(self):
-        """
-        Use xarray.save_mfdataset to write multiple netcdf files.
-        """
-        dar1 = xarray.DataArray(np.random.randn(2, 3))
-        dar2 = xarray.DataArray(np.random.randn(2, 3))
+        X_train = X_train[:10, :, :]
+        y_train = y_train[:10]
 
-        data1 = xarray.Dataset({'foo': dar1, 'bar': ('x', [1, 2])})
-        data2 = xarray.Dataset({'foo': dar2, 'bar': ('x', [1, 2])})
+        # Create a list of class folders to create
+        classes = np.unique(y_train)
 
-        file_names = [os.path.join(self.data_dir, f)
-                      for f in ['data1.nc', 'data2.nc']]
+        for cla in classes:
+            # Check that the folder exists
+            if not os.path.exists(os.path.join(self.data_dir, 'class{}')):
+                os.mkdir(os.path.join(self.data_dir, 'class{}'.format(cla)))
+        
+        for i, (x, y) in enumerate(zip(X_train, y_train)):
+            img_name = 'mnist{:0>2d}.jpg'.format(i+1)
+            fol = os.path.join(self.data_dir, 'class{}'.format(cla))
+            imageio.imwrite(os.path.join(fol, img_name))
 
-        xarray.save_mfdataset([data1, data2], file_names)
 
-        for f in file_names:
-            os.remove(f)
+
+        file_name = os.path.join(self.data_dir, "data.svmlight")
+        datasets.dump_svmlight_file(x, y, file_name,
+                                    comment="Sample svmlight file")
 
 
 if __name__ == "__main__":
-    XarraySample().invoke(sys.argv)
+    KerasSample().invoke(sys.argv)
